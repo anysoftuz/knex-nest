b7d159d926896b02198d043a1245cab4dea8e8e5
Shoxrux Rashidov <mrrashidov@hotmail.com>
2023-10-20 23:57:10 +0500

    fix(paginate|filter): added filter to search params && added count for paginations

--- lib/knex.magic.ts
+++ lib/knex.magic.ts
@@ -1,121 +1,160 @@
-import type {Knex} from 'knex';
-import {BaseResponse, CursorInterface, FilterParams, PageInfoInterface} from "./interfaces";
-
+import type { Knex } from "knex";
+import {
+  BaseResponse,
+  CursorInterface,
+  PageInfoInterface,
+  FilterParamsInterface,
+} from "./interfaces";
 
 export class KnexMagic {
-    /**
-     * @description filter data with given params and return query
-     * @param query { Knex.QueryBuilder }
-     * @param params { FilterParams }
-     */
-    public static filter(
-        query: Knex.QueryBuilder,
-        params: FilterParams,
-    ): Knex.QueryBuilder {
-        Object.entries<any>(params).forEach(([key, value]) => {
-            if (typeof value === 'object') {
-                Object.entries<any>(value).forEach(([subKey, subValue]) => {
-                    if (Array.isArray(subValue)) {
-                        query.whereIn(`${key}.${subKey}`, subValue);
-                    } else if (typeof subValue === 'object') {
-                        query.whereBetween(`${key}.${subKey}`, [
-                            subValue.min,
-                            subValue.max,
-                        ]);
-                    } else {
-                        query.where({[`${key}.${subKey}`]: subValue});
-                    }
-                });
-            } else {
-                if (Array.isArray(value)) {
-                    query.whereIn(key, value);
-                } else if (typeof value === 'object') {
-                    query.whereBetween(key, [value.min, value.max]);
-                } else {
-                    query.where({[key]: value});
-                }
-            }
-        });
-        return query;
+  /**
+   * @description filter data with given params and return query
+   * @param query { Knex.QueryBuilder }
+   * @param params { FilterParamsInterface }
+   */
+  public static filter(
+    query: Knex.QueryBuilder,
+    params?: FilterParamsInterface
+  ): Knex.QueryBuilder {
+    if (!params) {
+      return query;
     }
 
-    /**
-     * @description paginate data with cursor pagination method and return data with pagination metadata and total count of data
-     * @param query { Knex.QueryBuilder }
-     * @param cursorParams { CursorInterface }
-     * @param options { { key: string } }
-     */
-    public static async paginate<T>({
-                                        query,
-                                        cursorParams,
-                                        options,
-                                    }: CursorInterface): Promise<BaseResponse<T>> {
-        const cursorColumn = options.key || 'id';
-        const cursorColumnPrefix = options.keyPrefix || 'id';
-        const cursorId = Number(cursorParams.cursor || 0);
-        const cursorTake = Number(cursorParams.take || 10);
+    return Object.entries(params).reduce(
+      (query: Knex.QueryBuilder, [key, value]) => {
+        if (key === "search") {
+          const { columns, value: searchValue }: any = value;
 
-        const cursorDirection = cursorParams.direction || 'next';
-        const cursorMeta: PageInfoInterface = {
-            hasNextPage: false,
-            endCursor: null,
-            hasPreviousPage: false,
-            startCursor: null,
-        };
+          const searchConditions = columns.map((column, index) => {
+            const operator =
+              index === 0
+                ? "LOWER(" + column + ") LIKE ?"
+                : " OR LOWER(" + column + ") LIKE ?";
 
-        const whereOperator = this.getWhereOperator(cursorDirection);
+            return operator;
+          });
+          const likeValue: string = `%${searchValue.toLowerCase()}%`;
+          return query.whereRaw(
+            "(" + searchConditions.join("") + ")",
+            searchConditions.map(() => likeValue)
+          );
+        }
 
+        if (typeof value === "object") {
+          return Object.entries(value).reduce((query, [subKey, subValue]) => {
+            if (Array.isArray(subValue)) {
+              return query.whereIn(key + "." + subKey, subValue);
+            }
+            if (typeof subValue === "object") {
+              return query.whereBetween(key + "." + subKey, [
+                subValue.min,
+                subValue.max,
+              ]);
+            }
+            return query.andWhere({ [key + "." + subKey]: subValue });
+          }, query);
+        }
+
+        if (Array.isArray(value)) {
+          return query.whereIn(key, value);
+        }
+
+        return query.where(key, value);
+      },
+      query
+    );
+  }
+
+  /**
+   * @description paginate data with cursor pagination method and return data with pagination metadata and total count of data
+   * @param query { Knex.QueryBuilder }
+   * @param cursorParams { CursorInterface }
+   * @param options { { key: string } }
+   * @param callbackCountQuery { (result: any) => any }
+   */
+  public static async paginate<T>({
+    query,
+    cursorParams,
+    options,
+    callbackCountQuery,
+  }: CursorInterface): Promise<BaseResponse<T>> {
+    const cursorColumn = options.key || "id";
+    const cursorColumnPrefix = options.keyPrefix || "id";
+    const cursorId = Number(cursorParams.cursor || 0);
+    const cursorTake = Number(cursorParams.take || 10);
+
+    const cursorDirection = cursorParams.direction || "next";
+    const cursorMeta: PageInfoInterface = {
+      hasNextPage: false,
+      endCursor: null,
+      hasPreviousPage: false,
+      startCursor: null,
+    };
+
+    let totalCount: number = 0;
+    if (callbackCountQuery) {
+      const result = await callbackCountQuery(query);
+      totalCount = Number(result[0].count);
+    } else {
+      const countQuery = query.clone().clearSelect().count("id as count");
+      const result = await countQuery;
+      totalCount = Number(result[0].count);
+    }
+
+    const whereOperator = this.getWhereOperator(cursorDirection);
+
+    if (cursorParams.cursor) {
+      query.where(cursorColumnPrefix, whereOperator, cursorId);
+    }
+    const result: T[] = await query.limit(Number(cursorTake + 1));
+
+    if (result.length > cursorTake) {
+      if (cursorDirection === "next") {
+        cursorMeta.hasNextPage = true;
         if (cursorParams.cursor) {
-            query.where(cursorColumnPrefix, whereOperator, cursorId);
+          cursorMeta.hasPreviousPage = true;
+          cursorMeta.startCursor = result[0][cursorColumn];
         }
-        const result: T[] = await query.limit(Number(cursorTake + 1));
-
-        if (result.length > cursorTake) {
-            if (cursorDirection === 'next') {
-                cursorMeta.hasNextPage = true;
-                if (cursorParams.cursor) {
-                    cursorMeta.hasPreviousPage = true;
-                    cursorMeta.startCursor = result[0][cursorColumn];
-                }
-                cursorMeta.endCursor = result[result.length - 1][cursorColumn];
-            } else {
-                cursorMeta.hasPreviousPage = true;
-                if (cursorParams.cursor) {
-                    cursorMeta.hasNextPage = true;
-                    cursorMeta.endCursor = result[result.length - 1][cursorColumn];
-                }
-                cursorMeta.startCursor = result[0][cursorColumn];
-            }
-        } else if (result.length > 0 && result.length <= cursorTake) {
-            if (cursorDirection === 'next') {
-                if (cursorParams.cursor) {
-                    cursorMeta.hasPreviousPage = true;
-                    cursorMeta.endCursor = result[0][cursorColumn];
-                }
-            } else {
-                if (cursorParams.cursor) {
-                    cursorMeta.hasNextPage = true;
-                    cursorMeta.startCursor = result[result.length - 1][cursorColumn];
-                }
-            }
+        cursorMeta.endCursor = result[result.length - 1][cursorColumn];
+      } else {
+        cursorMeta.hasPreviousPage = true;
+        if (cursorParams.cursor) {
+          cursorMeta.hasNextPage = true;
+          cursorMeta.endCursor = result[result.length - 1][cursorColumn];
         }
-
-        return {
-            data: result,
-            pageInfo: cursorMeta,
-        };
+        cursorMeta.startCursor = result[0][cursorColumn];
+      }
+    } else if (result.length > 0 && result.length <= cursorTake) {
+      if (cursorDirection === "next") {
+        if (cursorParams.cursor) {
+          cursorMeta.hasPreviousPage = true;
+          cursorMeta.endCursor = result[0][cursorColumn];
+        }
+      } else {
+        if (cursorParams.cursor) {
+          cursorMeta.hasNextPage = true;
+          cursorMeta.startCursor = result[result.length - 1][cursorColumn];
+        }
+      }
     }
 
-    /**
-     * @description get where operator for cursor pagination
-     * @param direction
-     * @private
-     */
-    private static getWhereOperator(direction: string) {
-        if (direction === 'next') {
-            return '>';
-        } else {
-            return '<';
-        }
+    return {
+      data: result,
+      pageInfo: cursorMeta,
+      totalCount,
+    };
+  }
+
+  /**
+   * @description get where operator for cursor pagination
+   * @param direction
+   * @private
+   */
+  private static getWhereOperator(direction: string) {
+    if (direction === "next") {
+      return ">";
+    } else {
+      return "<";
     }
+  }
 }
